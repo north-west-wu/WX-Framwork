30d22c745e47d25536c5613639a3c2c3d326ecf4
wuxin <2939898502@qq.com>
2024-09-20 19:20:03 +0800

    提交部分UIGroup逻辑

---
+++ Assets/Scripts/UI/Interface/IUIGroup.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+namespace WXFramwork.UI
+{
+    public interface IUIGroup
+    {
+        /// <summary>
+        /// 界面组名称。
+        /// </summary>
+        string GroupName
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// 获取或设置界面组深度。
+        /// </summary>
+        int Depth
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// 获取或设置界面组是否暂停。
+        /// </summary>
+        bool Pause
+        {
+            get;
+            set;
+        }
+        
+        /// <summary>
+        /// 获取界面组中界面数量。
+        /// </summary>
+        int UIFormCount
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// 获取当前界面。
+        /// </summary>
+        IUIWindow CurrentUIWindow
+        {
+            get;
+        }
+
+        void Update();
+        
+        /// <summary>
+        /// 界面组中是否存在界面。
+        /// </summary>
+        bool HasUIWindow(int uiWindowId);
+        
+        /// <summary>
+        /// 放入 UI 界面
+        /// </summary>
+        void PushUIWindow(int uiWindowId);
+        
+        /// <summary>
+        /// 弹出 UI 界面
+        /// </summary>
+        void PopUIWindow(int uiWindowId);
+        
+        /// <summary>
+        /// 从界面组中获取所有界面。
+        /// </summary>
+        /// <returns>界面组中的所有界面。</returns>
+        IUIWindow[] GetAllUIWindows();
+    }
+}
---
+++ Assets/Scripts/UI/Interface/IUIWindow.cs
@@ -0,0 +1,46 @@
+﻿namespace WXFramwork.UI
+{
+    public interface IUIWindow
+    {
+        int WindowId
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// 界面名字
+        /// </summary>
+        string WindowName
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// 获取界面在界面组中的深度。
+        /// </summary>
+        int DepthInUIGroup
+        {
+            get;
+        }
+        
+        /// <summary>
+        /// 更新
+        /// </summary>
+        void Update();
+        
+        /// <summary>
+        /// 打开
+        /// </summary>
+        void Show();
+        
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        void Close();
+        
+        /// <summary>
+        /// 界面深度改变。
+        /// </summary>
+        void DepthChanged(int uiGroupDepth, int depthInUIGroup);
+    }
+}
